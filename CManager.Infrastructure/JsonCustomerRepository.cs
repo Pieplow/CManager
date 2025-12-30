59cf4c32f8bf4cbc6637aae21ab0cb19d9aaa318
Rasmus Pieplow <rasmus.pieplow@gmail.com>
2025-12-30 21:07:29 +0100

    json-customer-repository-complete

--- CManager.Infrastructure/JsonCustomerRepository.cs
+++ CManager.Infrastructure/JsonCustomerRepository.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.IO;
+using System.Text.Json;
 using System.Collections.Generic;
 using CManager.Core.Interfaces;
 using CManager.Core.Models;
@@ -7,14 +8,43 @@ namespace CManager.Infrastructure
 {
     public class JsonCustomerRepository : ICustomerRepository
     {
+        private readonly string _filePath;
+        public JsonCustomerRepository(string filePath = "Data/customers.json")
+        {
+            _filePath = filePath;
+        }
         public List<Customer> GetAll()
         {
-            throw new NotImplementedException();
+            if (!File.Exists(_filePath))
+            { 
+                
+               return new List<Customer>(); 
+            
+            }
+               
+            var json = File.ReadAllText(_filePath);
+            var customers = System.Text.Json.JsonSerializer.Deserialize<List<Customer>>(json);
+
+            return customers ?? new List<Customer>();
+
         }
 
         public bool SaveAll(List<Customer> customers)
         {
-            throw new NotImplementedException();
+            var directory = Path.GetDirectoryName(_filePath);
+
+            if (!string.IsNullOrEmpty(directory) && !Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            var json = System.Text.Json.JsonSerializer.Serialize(customers, new System.Text.Json.JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+
+            File.WriteAllText(_filePath, json);
+            return true;
         }
 
     }
